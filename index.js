7450512d51e1d71eb5b9f5a74e62e1879d0c3f3a
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-10-13 16:02:15 +0530

    Hardcoded Country code removed.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function sendClick() {
     var number = document.getElementById("number").value;
     var message = document.getElementById("message").value;
     message = message.split(' ').join('%20')
-    window.location = 'https://api.whatsapp.com/send?phone=91' + number + '&text=%20'+ message;
+    window.location = 'https://api.whatsapp.com/send?phone=' + number + '&text=%20'+ message;
 }
