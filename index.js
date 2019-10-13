c4c2d0e5f344d83394f299ac70d80e5b875dc4bc
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-10-13 14:01:30 +0530

    Intial checkin for whatapp message without storing user contact.

---
+++ index.js
@@ -0,0 +1,6 @@
+function sendClick() {
+    var number = document.getElementById("number").value;
+    var message = document.getElementById("message").value;
+    message = message.split(' ').join('%20')
+    window.location = 'https://api.whatsapp.com/send?phone=91' + number + '&text=%20'+ message;
+}
